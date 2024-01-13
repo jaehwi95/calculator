c9a79fcde6b2c11c73318bcda478a2a90ea00385
jaehwi95 <jaehwi95@gmail.com>
2024-01-13 21:15:35 +0900

    init project

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Jaehwi Kim on 2024/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
